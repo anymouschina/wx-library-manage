607a516661ce077cd0a6fadff58fb0fb0e411dae
unknown <624144061@qq.com>
2019-12-17 11:29:05 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,17 @@
 const app = getApp();
+import Dialog from '../../components/dialog/dialog.js';
 Page({
   data: {
     active: 0,
     avatarUrl: './user-unlogin.png',
     userInfo: {},
     list: {},
+    showLoading:false,
     timer:null,//节流器
     page:1,
     expoIdx:0,
     bookList:[],
+    defaultList:[],
     logged: false,
     takeSession: false,
     requestResult: '',
@@ -29,7 +32,7 @@ Page({
       sbInfo: false
     }
   },
-  onShow: function () {
+  onLoad: function () {
     this.getList()
   },
   interSection: function (e) {
@@ -46,6 +49,7 @@ Page({
         bottom: 0
       })
       .observe('.item', (item) => {
+        console.log(item)
         if(this.data.timer){
           clearTimeout(this.data.timer)
         }
@@ -57,9 +61,22 @@ Page({
       })
   },
   onPageScroll(event){
+    const that = this
+    wx.createSelectorQuery().selectAll('.book-item').boundingClientRect(function (rect) {
+      if(rect[0].height - event.scrollTop===450){
+        that.setData({
+          page:that.data.page+1
+        })
+        that.getList()
+      }
+    // console.log(rect)
+    }).exec()
     this.interSection(event)
   },
   getList(){
+    this.setData({
+      showLoading: true
+    })
     app.globalData.request({
       url: `/goods/findListPage`,
       method: 'GET',
@@ -72,12 +89,23 @@ Page({
       success: res => {
         // res.data 为服务端正确登录后签发的 JWT
         this.setData({
-          bookList:res.data.data
+          showLoading: false
         })
+        let list = this.data.bookList
+        const ids = list.map(item => item.id)
+        let isRepeat = res.data.data.some(item=>ids.indexOf(item.id)>-1)
+        if(!isRepeat){
+          list.push(...res.data.data)
+          this.setData({
+            bookList: list,
+            defaultList:list
+          })
+        }
       }
     })
   },
   scanSbInfo(){
+    let that = this
     wx.scanCode({
       success(res) {
         app.globalData.open_id && (app.globalData.request({
@@ -88,7 +116,15 @@ Page({
           },
           success: res => {
             // res.data 为服务端正确登录后签发的 JWT
-            console.log(res)
+            if(res.data.error){
+              wx.showToast({
+                title: res.data.error,
+                icon: 'info',
+                iconColor: 'green'
+              })
+            }else{
+              that.scanSbInfo()
+            }
           }
         }))
       }
@@ -96,8 +132,20 @@ Page({
   },
   onInputChange(e) {
     this.setData({
-      bookName: e.detail
+      bookList: e.detail === ''?this.data.defaultList:this.data.defaultList.filter(item=>item.goodsName.indexOf(e.detail)>-1)
     });
+  },
+  queryObjParse(obj){//obj装url的query
+   let arr = []
+    for(let key in obj){
+      arr.push(`${key}=${this.toString.call(obj[key]) === '[object Object]' || this.toString.call(obj[key]) === '[object Array]'?JSON.stringify(obj[key]):obj[key]}`)
+    }
+    return arr.join('&')
+  },
+  lookDetail(item){//查看详情
+    wx.navigateTo({
+      url: '/pages/detail/detail?'+this.queryObjParse(item.mark.detail)
+    })
   },
   onInputSearch() {
     wx.showToast({title:'搜索' + this.data.bookName});
